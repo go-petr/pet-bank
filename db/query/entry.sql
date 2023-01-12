6f4616bc13b078a84c19c52ff2da7164bc8c0909
peter <gorkovenko.petr@gmail.com>
2023-01-12 17:00:51 +0400

    Add unit tests for account, entry and transfer CRUD queries

--- db/query/entry.sql
+++ db/query/entry.sql
@@ -11,14 +11,5 @@ WHERE id = $1 LIMIT 1;
 
 -- name: ListEntries :many
 SELECT * FROM entries
-LIMIT $1 OFFSET $2;
-
--- name: UpdateEntry :one
-UPDATE entries
-SET amount = $2
-WHERE id = $1
-RETURNING *;
-
--- name: DeleteEntry :exec
-DELETE FROM entries
-WHERE id = $1;
+WHERE account_id = $1
+LIMIT $2 OFFSET $3;
---
+++ util/random.go
@@ -0,0 +1,48 @@
+package util
+
+import (
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+// RandomInt generates a random integer between min and max
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// RandomString generates a random string of length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+// RandomOwner generates a random owner name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+// RandomMoneyAmount generates a random amount of money
+func RandomMoneyAmount() string {
+	return strconv.Itoa(int(RandomInt(0, 1000)))
+}
+
+// RandomCurrency generates a random currency code
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "RMB"}
+	return currencies[rand.Intn(len(currencies))]
+}
